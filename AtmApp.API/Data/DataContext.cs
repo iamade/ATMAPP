772a3eea12cdd4fbffd6205aba965ff1230311bf
kamar <kamar3deen@gmail.com>
2019-09-15 10:16:51 +0100

    inital Commit

---
+++ AtmApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using AtmApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
