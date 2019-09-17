870299efcaab0c25f98cf25f7b62ff23de3f0c1f
iamade <31669931+iamade@users.noreply.github.com>
2019-09-17 17:25:06 +0100

    AuthRepo

--- AtmApp.API/Data/DataContext.cs
+++ AtmApp.API/Data/DataContext.cs
@@ -7,7 +7,8 @@ namespace AtmApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
         
-
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ AtmApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using AtmApp.API.Models;
+
+namespace AtmApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
