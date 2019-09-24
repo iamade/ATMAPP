ffc00d252cddbe249e0f684efbad58b745033811
iamade <31669931+iamade@users.noreply.github.com>
2019-09-24 11:14:07 +0100

    End of Bootstuff

--- AtmApp-SPA/src/app/app.module.ts
+++ AtmApp-SPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -25,7 +26,9 @@ import { RegisterComponent } from './register/register.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
+
    ],
    providers: [
       AuthService,
